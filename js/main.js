5113f1745baa2ac4d09df1da810f07b7bf2e1376
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-03-24 18:15:15 +0200

    оиашняя работа2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const mySwiper = new Swiper('.swiper-container', {
 	loop: true,
-
 	// Navigation arrows
 	navigation: {
 		nextEl: '.slider-button-next',
@@ -29,7 +28,7 @@ const escapeHandler = (event) => {
 }
 overlay.addEventListener('click', (event) => {
 	const target = event.target;
-	if (target.classList.contains('modal-close') || target === modalCart) {
+	if (target.classList.contains('modal-close') || target.classList.contains('overlay')) {
 		closeModal();
 	}
 })
@@ -38,17 +37,118 @@ buttonCart.addEventListener('click', openModal);
 
 //Плавная прокрутка
 // на все ссылки получаем клас что бы прикрутить к ним прокрутку
+/* {
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+	for (let i = 0; i < scrollLinks.length; i++) {
+		scrollLinks[i].addEventListener('click', (event) => {
+			event.preventDefault();  // убираем стандартное браузерное поведение - скачек
+			const id = scrollLinks[i].getAttribute("href");
+			document.querySelector(id).scrollIntoView({   // устанавливаем прокрутку (метод scrollIntoView не работает в старых браузерах)
+				behavior: 'smooth', // тип прокрутки плавный 
+				block: 'start',      // до куда прокрутка - в начало
+			})
+		})
+	}
+} */
 {
 	const scrollLinks = document.querySelectorAll('a.scroll-link');
 
-for (let i = 0; i < scrollLinks.length; i++){
-	scrollLinks[i].addEventListener('click', (event) => {
-		event.preventDefault();  // убираем стандартное браузерное поведение - скачек
-		const id = scrollLinks[i].getAttribute("href");
-		document.querySelector(id).scrollIntoView({   // устанавливаем прокрутку (метод scrollIntoView не работает в старых браузерах)
-			behavior: 'smooth', // тип прокрутки плавный 
-			block:'start',      // до куда прокрутка - в начало
-		}) 
-	})
+	for (const scrollLink of scrollLinks) {
+		scrollLink.addEventListener('click', (event) => {
+			event.preventDefault();  // убираем стандартное браузерное поведение - скачек
+			const id = scrollLink.getAttribute("href");
+			document.querySelector(id).scrollIntoView({   // устанавливаем прокрутку (метод scrollIntoView не работает в старых браузерах)
+				behavior: 'smooth', // тип прокрутки плавный 
+				block: 'start',      // до куда прокрутка - в начало
+			})
+		})
 	}
 }
+
+//товары
+const more = document.querySelector(".more");
+const navigationLink = document.querySelectorAll('.navigation-link');
+const longGoodsList = document.querySelector('.long-goods-list');
+const viewAll = document.querySelectorAll('.view-all');
+
+const getGoods = async function () {
+	const result = await fetch('./../db/db.json');
+	if (!result.ok) {
+		throw 'Ошибочка вышла' + result.status;  //какая ошибка 
+	}
+	return await result.json(); //метод json() превращает строку или масив в джейсон
+};
+
+const createCard = function ({label,img,name,description,id,price}) {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6';  //можно и через класлист эдд  через запятую добавить классы
+	
+	card.innerHTML = `
+	<div class="goods-card">
+						${label ?
+						`<span class="label">${label}</span>` :
+						''}
+						<img src="db/${img}" alt="${name}" class="goods-image">
+						<h3 class="goods-title">${name}</h3>
+						<p class="goods-description">${description}</p>
+						<button class="button goods-card-btn add-to-cart" data-id="${id}">
+							<span class="button-price">$${price}</span>
+						</button>
+					</div>`;
+	return card;
+}
+
+const renderCard = function(data){
+	longGoodsList.textContent = '';    //очещаем верстку
+	const cards = data.map(createCard);
+/* 	cards.forEach(card => {
+		longGoodsList.append(card)
+	}); */
+	longGoodsList.append(...cards);
+	document.body.classList.add('show-goods');
+}
+
+//ДЗ плавная прокрутка вверх
+more.addEventListener('click', (event) => {
+	event.preventDefault();
+	getGoods().then(renderCard);
+	const id = more.getAttribute("href");
+			document.querySelector(id).scrollIntoView({   // устанавливаем прокрутку (метод scrollIntoView не работает в старых браузерах)
+				behavior: 'smooth', // тип прокрутки плавный 
+				block: 'start',      // до куда прокрутка - в начало
+			})
+ })
+
+const filterCards = function (field, value) {
+	getGoods()
+		.then(function (data) {
+			const filteredGoods = data.filter(function (good) {
+				return good[field] === value;
+			});
+			return filteredGoods;
+		})
+		.then(renderCard);
+}
+//ДЗ фильтр товаров 
+viewAll.forEach(link => {
+	link.addEventListener('click', () => {
+		const value = link.dataset.field;
+		console.log(value);
+			filterCards("category", value);
+	})
+})
+//ДЗ добавила условие для кнопки показать все
+navigationLink.forEach(link => {
+	link.addEventListener('click', (event) => {
+		event.preventDefault();
+		const field = link.dataset.field;   // что бы получить датаатрибут у линка
+		const value = link.textContent;
+		console.log(field, value);
+		if (field) {
+			filterCards(field, value);
+		}else{
+		getGoods().then(renderCard);
+		}
+	})
+})
